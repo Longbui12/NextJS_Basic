69981a6f8e6d1012d804228d2436700db6dcff55
rn25-long-app <bhlong3112@gmail.com>
2024-05-17 15:44:37 +0700

    Design Modal Add New vs bootstrap , use props to parent to child, handle events

--- src/components/app.table.tsx
+++ src/components/app.table.tsx
@@ -2,6 +2,8 @@
 import Table from "react-bootstrap/Table";
 import { Button } from "react-bootstrap";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import CreateModal from "./create.modal";
+import { useState } from "react";
 
 interface IProps {
   blogs: IBlog[];
@@ -11,43 +13,59 @@ function AppTable(props: IProps) {
   const { blogs } = props;
   console.log(">>>> Check props blogs :", blogs);
 
+  const [showModalCreate, setShowModalCreate] = useState<boolean>(false);
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Title</th>
-          <th>Author</th>
-          <th>Action</th>
-          <th>Content</th>
-        </tr>
-      </thead>
-      <tbody>
-        {blogs?.map((blog) => {
-          return (
-            <tr key={blog.id}>
-              <td>{blog.id}</td>
-              <td>{blog.title}</td>
-              <td>{blog.author}</td>
-              <td>
-                <ButtonGroup vertical>
-                  <Button variant="success" className="my-3">
-                    View
-                  </Button>
-                  <Button variant="warning" className="my-3">
-                    Edit
-                  </Button>
-                  <Button variant="danger" className="my-3">
-                    delete
-                  </Button>
-                </ButtonGroup>
-              </td>
-              <td>{blog.content}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+    <>
+      <div
+        className="mb-3"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <h3>Table Blogs</h3>
+        <Button variant="secondary" onClick={() => setShowModalCreate(true)}>
+          Add new
+        </Button>
+      </div>
+      <Table bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogs?.map((blog) => {
+            return (
+              <tr key={blog.id}>
+                <td>{blog.id}</td>
+                <td>{blog.title}</td>
+                <td>{blog.author}</td>
+                <td>
+                  <ButtonGroup vertical>
+                    <Button variant="success" className="my-3">
+                      View
+                    </Button>
+                    <Button variant="warning" className="my-3">
+                      Edit
+                    </Button>
+                    <Button variant="danger" className="my-3">
+                      delete
+                    </Button>
+                  </ButtonGroup>
+                </td>
+                <td>{blog.content}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+      <CreateModal
+        showModalCreate={showModalCreate}
+        setShowModalCreate={setShowModalCreate}
+      />
+    </>
   );
 }
 
---
+++ src/components/create.modal.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+
+interface IProps {
+  showModalCreate: boolean;
+  setShowModalCreate: (value: boolean) => void;
+}
+
+function CreateModal(props: IProps) {
+  const { showModalCreate, setShowModalCreate } = props;
+  const [title, setTitle] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const handleSubmit = () => {
+    console.log("CHeck data :", title, author, content);
+  };
+
+  // This funtion to when close Modal will clear data :
+  const handleCloseModal = () => {
+    setTitle(""), setAuthor(""), setContent(""), setShowModalCreate(false);
+  };
+  return (
+    <>
+      <Modal
+        show={showModalCreate}
+        onHide={() => handleCloseModal()}
+        backdrop="static"
+        keyboard={false}
+        size="lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add New A Blog</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="..."
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Author</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="..."
+                value={author}
+                onChange={(event) => setAuthor(event.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Content</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="..."
+                value={content}
+                onChange={(event) => setContent(event.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleCloseModal()}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handleSubmit()}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default CreateModal;
